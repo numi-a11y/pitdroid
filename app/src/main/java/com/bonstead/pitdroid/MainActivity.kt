3e90293eaf05875c0b2548861c21b296495c9022
numi-a11y <numi@plockepinn.org>
2026-07-09 15:35:58 +0200

    more error fixes

--- app/src/main/java/com/bonstead/pitdroid/MainActivity.kt
+++ app/src/main/java/com/bonstead/pitdroid/MainActivity.kt
@@ -2,15 +2,36 @@ package com.bonstead.pitdroid
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState) // Fix: Lade till parent-anropet korrekt
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Starta med instrumentpanelen som standardvy
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, DashFragment())
-            .commit()
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        // Hantera klick i bottenmenyn
+        bottomNavigation.setOnItemSelectedListener { item ->
+            val selectedFragment = when (item.itemId) {
+                R.id.navigation_tenderness -> TendernessFragment()
+                R.id.navigation_dashboard -> DashFragment()
+                R.id.navigation_settings -> SettingsFragment()
+                else -> TendernessFragment()
+            }
+
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, selectedFragment)
+                .commit()
+            true
+        }
+
+        // Sätt Tenderness som startskärm vid första uppstarten
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, TendernessFragment())
+                .commit()
+        }
     }
 }
--- app/src/main/java/com/bonstead/pitdroid/SettingsFragment.kt
+++ app/src/main/java/com/bonstead/pitdroid/SettingsFragment.kt
@@ -1,7 +1,26 @@
 package com.bonstead.pitdroid
-import android.os.Bundle
-import androidx.fragment.app.Fragment
 
-class SettingsFragment : Fragment() {
-    // Temporärt tom inställningsvy
+import android.content.SharedPreferences
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        preferenceScreen.sharedPreferences?.registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        preferenceScreen.sharedPreferences?.unregisterOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+        // Android sköter sparandet automatiskt
+    }
 }
